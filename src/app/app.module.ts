bd55eb771cf405ad4126cd4db467f04861ea09b1
Simbadev <maxime.gasnier.1@gmail.com>
2017-03-08 18:42:21 +0100

    add bottom-bart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule,
