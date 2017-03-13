ec4ea7e37af874f1c154592f1ef3708944aaea69
Simbadev <maxime.gasnier.1@gmail.com>
2017-03-13 18:01:19 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import { TransactionService } from "app/service/transaction.service";
 import { ShopService } from "app/service/shop.service";
 import { ProductService } from "app/service/product.service";
 import { SellerService } from "app/service/seller.service";
+import { PanelComponent } from './components/panel/panel.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
