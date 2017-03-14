ed9c156616d2daf011832e489ede44579e600a91
Simbadev <maxime.gasnier.1@gmail.com>
2017-03-14 13:00:00 +0100

    configure project for scss

--- src/app/components/panel/catalog/catalog.component.ts
+++ src/app/components/panel/catalog/catalog.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
 
--- src/app/components/panel/ticket/ticket.component.ts
+++ src/app/components/panel/ticket/ticket.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
-  styleUrls: ['./ticket.component.css']
+  styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent implements OnInit {
 
