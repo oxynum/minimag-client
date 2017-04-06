eca4baf2abcf3586fb70c06935d3236e17d8dd1e
Simbadev <maxime.gasnier.1@gmail.com>
2017-04-06 21:10:45 +0200

    add cmean style

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -8,11 +8,69 @@ import { Component, OnInit } from '@angular/core';
 export class PanelComponent implements OnInit {
   public topComponent: string;
   public bottomComponent: string;
+  private itemsForMiniDropDown: Object[] = [{action: 'AJOUTER'}, {action: 'VOIR'}];
+  public articles: number = 0;
+  private items: Array<Object>;
 
-  constructor() { }
+  constructor() {
+    this.items = [
+      {
+        "EAN": "7896541238526",
+        "Name": "Sweet sauce",
+        "Price": "10",
+        "Promo": "En soldes",
+        "State": "En stock",
+        "Attribute": "80cm"
+      },
+      {
+        "EAN": "7896541238526",
+        "Name": "Sweet sauce",
+        "Price": "10",
+        "Promo": "En soldes",
+        "State": "En stock",
+        "Attribute": "80cm"
+      },
+      {
+        "EAN": "7896541238526",
+        "Name": "Sweet sauce",
+        "Price": "10",
+        "Promo": "En soldes",
+        "State": "En stock",
+        "Attribute": "80cm"
+      },
+      {
+        "EAN": "7896541238526",
+        "Name": "Sweet sauce",
+        "Price": "10",
+        "Promo": "En soldes",
+        "State": "En stock",
+        "Attribute": "80cm"
+      },
+      {
+        "EAN": "7896541238526",
+        "Name": "Sweet sauce",
+        "Price": "10",
+        "Promo": "En soldes",
+        "State": "En stock",
+        "Attribute": "80cm"
+      },
+      {
+        "EAN": "7896541238526",
+        "Name": "Sweet sauce",
+        "Price": "10",
+        "Promo": "En soldes",
+        "State": "En stock",
+        "Attribute": "80cm"
+      },
+    ]
+   }
 
   ngOnInit() {
 
   }
 
+  addToArticles(event) {
+    this.articles++;
+  }
+  
 }
